d38214adbe98212fe66ead9ee7fc6254c2901c1f
Joshua Morin <122689763+IDGS-803-19002318@users.noreply.github.com>
2023-03-15 20:55:33 -0600

    Add crud to Students and Teachers
    Finish app escuela

--- myapp/app.py
+++ myapp/app.py
@@ -1,14 +1,17 @@
 import flask
-
 from Alumnos.routes import alumnos
-from Directivos.routes import dire
 from Maestros.routes import maestros
+from Directivos.routes import dire
+from flask_wtf.csrf import CSRFProtect
+from models import db
+from config import DevelopmentConfig
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect()
 
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return flask.jsonify({'Datos': 'Home'})
 
@@ -17,5 +20,10 @@ app.register_blueprint(alumnos)
 app.register_blueprint(maestros)
 app.register_blueprint(dire)
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    csrf.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(port=3000)
